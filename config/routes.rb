60df5245bbf6b07bbbf06bed191741c1dad13917
CourtneyNicole <courtney_nicole_flynn@yahoo.com>
2019-07-12 01:27:11 +0000

    enrollemtn

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,18 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'static_pages#index'
-  resources :courses, only: [:index, :show]
+  resources :courses, only: [:index, :show] do
+    resources :enrollments, only: :create
+  end
+
   resources :lessons, only: [:show]
   namespace :instructor do
     resources :sections, only: [] do
       resources :lessons, only: [:new, :create]
     end
     resources :courses, only: [:new, :create, :show] do
-    resources :sections, only: [:new, :create]  do
-    resources :images, only: [:new, :create, :show]
-    end
+    resources :sections, only: [:new, :create] 
+   
    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 end
